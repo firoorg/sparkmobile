35d81b2386863597ddddd89119ec89fa9acbd6c3
sneurlax <sneurlax@gmail.com>
2023-11-08 14:49:51 -0600

    update createSpendKey
    TODO remove it

--- src/dart_interface.cpp
+++ src/dart_interface.cpp
@@ -5,84 +5,54 @@
 
 using namespace spark;
 
-/// Generate a new spend key and cast it to and FFI-friendly string.
-const char * generateSpendKey() {
+/// Create a SpendKey from a given keyData and index.
+///
+/// keyData is derived using BIP44 from a seed.
+const char * createSpendKey(const char *keyData, int index) {
     try {
+        // Convert the keyData from hex string to binary
+        unsigned char* key_data_bin = hex2bin(keyData);
+
         // Use the default (deployment) parameters.
         const spark::Params *params = spark::Params::get_default();
 
-        // Generate a new SpendKey.
-        spark::SpendKey key(params);
+        // Generate r from keyData and index.
+        std::string nCountStr = std::to_string(index);
+        CHash256 hasher;
+        std::string prefix = "r_generation";
+        hasher.Write(reinterpret_cast<const unsigned char*>(prefix.c_str()), prefix.size());
+        hasher.Write(key_data_bin, std::strlen(keyData) / 2); // divide by 2 because it's hex string (2 chars for 1 byte)
+        hasher.Write(reinterpret_cast<const unsigned char*>(nCountStr.c_str()), nCountStr.size());
+        unsigned char hash[CSHA256::OUTPUT_SIZE];
+        hasher.Finalize(hash);
 
-        // Instead of just returning the SpendKey, we need to cast it to an FFI-friendly string.
-        // The r value is used to create a SpendKey.  Use get_r to get the r value.
-        const secp_primitives::Scalar &r = key.get_r();
+        // Create a Scalar from the hash.
+        secp_primitives::Scalar r_scalar;
+        r_scalar.memberFromSeed(hash);
 
-        // Allocate a buffer of the required size.
-        unsigned char serialized_r_buffer[32]; // Assuming size is 32, adjust if necessary
-
-        // Pass this buffer to the r.serialize() method.
-        r.serialize(serialized_r_buffer);
-
-        // Fill the std::vector<unsigned char> with the serialized data.
-        std::vector<unsigned char> serialized_r(serialized_r_buffer,
-                                                serialized_r_buffer + sizeof(serialized_r_buffer));
-
-        // Cast the serialized r value to an FFI-friendly string.
-        const char *serialized_r_str = bin2hex(serialized_r.data(), serialized_r.size());
-
-        // Return the serialized r value.
-        return serialized_r_str;
-    } catch (const std::exception& e) {
-        // If an exception is thrown, print it to the console.
-        std::cout << "Exception: " << e.what() << "\n";
-
-        // Return the error message.
-        return e.what();
-    }
-}
-
-/// Create a SpendKey from a given r.
-//extern "C" __attribute__((visibility("default"))) __attribute__((used))
-const char * createSpendKey(const char *r) {
-    try {
-        // Use the default (deployment) parameters.
-        const spark::Params *params = spark::Params::get_default();
-
-        // Get the unsigned char* data from hex2bin.
-        unsigned char* r_data = hex2bin(r);
-
-        // Create a Scalar from the r data.
-        secp_primitives::Scalar r_scalar(r_data);
-
-        // Create a SpendKey from the r scalar.
+        // Create a SpendKey from the Scalar.
         spark::SpendKey key(params, r_scalar);
 
-        // Instead of just returning the SpendKey, we need to cast it to an FFI-friendly string.
-        // The r value is used to create a SpendKey.  Use get_r to get the r value.
-        const secp_primitives::Scalar &r = key.get_r();
+        // Serialize the SpendKey or its components to an FFI-friendly format.
+        // Assuming you need to serialize the Scalar r from the SpendKey,
+        // and that the serialize method populates an unsigned char array.
+        unsigned char serialized_r_buffer[32]; // Replace 32 if the size is different
+        r_scalar.serialize(serialized_r_buffer);
 
-        // Allocate a buffer of the required size.
-        unsigned char serialized_r_buffer[32]; // Assuming size is 32, adjust if necessary
+        // Convert the serialized data to a hex string.
+        char *serialized_r_str = bin2hex(serialized_r_buffer, sizeof(serialized_r_buffer));
 
-        // Pass this buffer to the r.serialize() method.
-        r.serialize(serialized_r_buffer);
+        // Free the allocated memory for key_data_bin
+        delete[] key_data_bin;
 
-        // Fill the std::vector<unsigned char> with the serialized data.
-        std::vector<unsigned char> serialized_r(serialized_r_buffer,
-                                                serialized_r_buffer + sizeof(serialized_r_buffer));
-
-        // Cast the serialized r value to an FFI-friendly string.
-        const char *serialized_r_str = bin2hex(serialized_r.data(), serialized_r.size());
-
-        // Return the serialized r value.
+        // Return the hex string. Caller is responsible for freeing this memory.
         return serialized_r_str;
-    } catch (const std::exception& e) {
-        // If an exception is thrown, print it to the console.
-        std::cout << "Exception: " << e.what() << "\n";
 
-        // Return the error message.
-        return e.what();
+    } catch (const std::exception& e) {
+        // If an exception is thrown, return a static error message.
+        // Note: We are returning a string literal here because it is managed by the system
+        // and doesn't need to be freed by the caller.
+        return "Exception occurred";  // Or use e.what() if the caller can handle dynamic allocation.
     }
 }
 
