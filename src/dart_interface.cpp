bae5e435a8facf8d12f693c2731ff5b0b5f195c6
sneurlax <sneurlax@gmail.com>
2023-11-08 16:27:42 -0600

    dart ffigen doesn't support uint64_t
    but if we need to support a diversifier above  2,147,483,647, we will have to split the 64-bit BigInt diversifier in Dart into two 32-bit int32s.

--- src/dart_interface.cpp
+++ src/dart_interface.cpp
@@ -7,13 +7,22 @@
 using namespace spark;
 
 /// FFI-friendly wrapper for spark:getAddress.
-const char* getAddress(const char* keyData, int index, uint64_t diversifier) {
+const char* getAddress(const char* keyData, int index, int diversifier) {
+// To support a diversifier above 2,147,483,647, use the definition below.
+// const char* getAddress(const char* keyData, int index, int32_t diversifier_high, int32_t diversifier_low) {
     try {
+        // The diversifier is cast to uint64_t if the underlying implementation requires it.
+        // This assumes that the diversifier being passed can fit within the range of uint64_t.
+        uint64_t diversifier_cast = static_cast<uint64_t>(diversifier);
+
+        // Combine the two 32-bit values into a single 64-bit unsigned integer.
+        // uint64_t diversifier_cast = (static_cast<uint64_t>(diversifier_high) << 32) | (static_cast<uint32_t>(diversifier_low) & 0xFFFFFFFFULL);
+
         // Convert keyData to Address object using getAddressFromData.
         spark::SpendKey spendKey = createSpendKeyFromData(keyData, index);
         spark::FullViewKey fullViewKey(spendKey);
         spark::IncomingViewKey incomingViewKey(fullViewKey);
-        spark::Address address(incomingViewKey, diversifier);
+        spark::Address address(incomingViewKey, diversifier_cast);
 
         // Encode the Address object into a string.
         std::string encodedAddress = address.encode(ADDRESS_NETWORK_TESTNET);
--- src/dart_interface.h
+++ src/dart_interface.h
@@ -1,7 +1,7 @@
 #ifndef ORG_FIRO_SPARK_DART_INTERFACE_H
 #define ORG_FIRO_SPARK_DART_INTERFACE_H
 
-const char* getAddress(const char* keyData, int index, uint64_t diversifier);
+const char* getAddress(const char* keyData, int index, int diversifier);
 
 /*
 const char *createFullViewKey(const char* keyData, int index);
