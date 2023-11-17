c453a946d8b8f3e968b07a8b7a4b205f80fcf527
sneurlax <sneurlax@gmail.com>
2023-11-17 16:20:38 -0600

    move C++-style constructor to implementation
    so ffigen plays with it nicely

--- src/dart_interface.cpp
+++ src/dart_interface.cpp
@@ -44,6 +44,53 @@ const char* getAddress(const char* keyDataHex, int index, int diversifier, int i
     }
 }
 
+struct CCoin {
+    const char type;
+    const unsigned char* k;
+    int kLength;
+    const char* keyData;
+    int index;
+    uint64_t v;
+    const unsigned char* memo;
+    int memoLength;
+    const unsigned char* serial_context;
+    int serial_contextLength;
+
+    // Constructor
+    CCoin(char type,
+          const unsigned char* k, int kLength,
+          const char* keyData,
+          int index,
+          uint64_t v,
+          const unsigned char* memo, int memoLength,
+          const unsigned char* serial_context, int serial_contextLength)
+            : type(type),
+              k(copyBytes(k, kLength)), kLength(kLength),
+              keyData(strdup(keyData)),
+              index(index),
+              v(v),
+              memo(copyBytes(memo, memoLength)), memoLength(memoLength),
+              serial_context(copyBytes(serial_context, serial_contextLength)),
+              serial_contextLength(serial_contextLength) {}
+
+    // Destructor
+    ~CCoin() {
+        delete[] k;
+        delete[] keyData;
+        delete[] memo;
+        delete[] serial_context;
+    }
+
+private:
+    // Helper function for deep copying byte arrays
+    static unsigned char* copyBytes(const unsigned char* source, int length) {
+        if (source == nullptr) return nullptr;
+        unsigned char* dest = new unsigned char[length];
+        std::memcpy(dest, source, length);
+        return dest;
+    }
+};
+
 /*
  * FFI-friendly wrapper for spark:identifyCoin.
  *
