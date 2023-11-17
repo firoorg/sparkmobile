78057f7384a678c3d002521e67339c9d76ffc5b5
sneurlax <sneurlax@gmail.com>
2023-11-17 12:38:50 -0600

    add CCoin constructor

--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,8 @@
 #include "../include/spark.h"
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 const char* getAddressFromData(const char* keyData, int index, const uint64_t diversifier);
 
@@ -20,6 +22,40 @@ struct CCoin {
 	int memoLength;
 	const unsigned char* serial_context;
 	int serial_contextLength;
+
+	// Constructor
+	CCoin(char type,
+		  const unsigned char* k, int kLength,
+		  const char* keyData,
+		  int index,
+		  uint64_t v,
+		  const unsigned char* memo, int memoLength,
+		  const unsigned char* serial_context, int serial_contextLength)
+			: type(type),
+			  k(copyBytes(k, kLength)), kLength(kLength),
+			  keyData(strdup(keyData)),
+			  index(index),
+			  v(v),
+			  memo(copyBytes(memo, memoLength)), memoLength(memoLength),
+			  serial_context(copyBytes(serial_context, serial_contextLength)),
+			  serial_contextLength(serial_contextLength) {}
+
+	// Destructor
+	~CCoin() {
+		delete[] k;
+		delete[] keyData;
+		delete[] memo;
+		delete[] serial_context;
+	}
+
+private:
+	// Helper function for deep copying byte arrays
+	static unsigned char* copyBytes(const unsigned char* source, int length) {
+		if (source == nullptr) return nullptr;
+		unsigned char* dest = new unsigned char[length];
+		std::memcpy(dest, source, length);
+		return dest;
+	}
 };
 
 spark::Coin convertToCppStruct(const CCoin& c_struct);
