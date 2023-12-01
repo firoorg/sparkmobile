70ce5119c21f8b20a0488b5869e571e4ef0ece8e
sneurlax <sneurlax@gmail.com>
2023-11-30 18:17:40 -0600

    add WIP cCreateSparkSpendTransaction
    sparkmobile's already squatting createSparkSpendTransaction D:<

--- src/dart_interface.cpp
+++ src/dart_interface.cpp
@@ -2,6 +2,7 @@
 #include "utils.h"
 
 #include <cstring>
+#include "../bitcoin/uint256.h"
 #include <iostream> // Just for printing.
 
 #ifndef EXPORT_DART
@@ -127,4 +128,173 @@ struct CCRecipient* createSparkMintRecipients(
 
     // Return the array.
     return cRecipients;
+}
+
+/*
+ * FFI-friendly wrapper for spark::createSparkSpendTransaction.
+ *
+ * createSparkSpendTransaction is the longest and most complicated function to be wrapped.  It
+ * accepts several sparkmobile-specific structs which have been wrapped here and in utils.cpp.
+ * These structs are then converted to the corresponding C++ structs and passed to the C++ function
+ * spark::createSparkSpendTransaction.  The return signature is void and the result is written to
+ * the serializedSpend parameter.
+ *
+ * Instead of accepting spend, full view, and incoming view keys, we accept the key data and index
+ * and derive the keys here.  We could alternatively serialize and save keys to memory or disk and
+ * deserialize them as needed, which would enable watch-only wallets, but this is simpler for now.
+ * The rest of the parameters are relatively straightforward to convert from their C, FFI-friendly
+ * structs to their C++ counterparts, with the exception of idAndBlockHashes_all, which doesn't
+ * appear to be used in sparkmobile's createSparkSpendTransaction, so we ignore it and pass an empty
+ * vector.
+ *
+ * The serializedSpend parameter is where the result is written.  We allocate memory for it here,
+ * then copy the data from the vector to an array which is returned.  The caller is responsible for
+ * freeing the memory.
+ *
+ * The original function is here:
+ *
+ * void createSparkSpendTransaction(
+ * const spark::SpendKey& spendKey,
+ * const spark::FullViewKey& fullViewKey,
+ * const spark::IncomingViewKey& incomingViewKey,
+ * const std::vector<std::pair<CAmount, bool>>& recipients,
+ * const std::vector<std::pair<spark::OutputCoinData, bool>>& privateRecipients,
+ * std::list<CSparkMintMeta> coins,
+ * const std::unordered_map<uint64_t, spark::CoverSetData> cover_set_data_all,
+ * const std::map<uint64_t, uint256>& idAndBlockHashes_all,
+ * const uint256& txHashSig,
+ * CAmount &fee,
+ * std::vector<uint8_t>& serializedSpend,
+ * std::vector<std::vector<unsigned char>>& outputScripts)
+ *
+ * The SpendKey, FullViewKey, and IncomingViewKey are replaced with the key data and index.
+ * CAmounts are trivially-constructable from uint64_t.  We have wrapped OutputCoinData,
+ * CSparkMintMeta, and CoverSetData as COuputCoinData, CCSparkMintMeta, and CCoverSetData,
+ * respectively.  idAndBlockHashes_all is not used in sparkmobile's createSparkSpendTransaction,
+ * so we ignore it and pass an empty vector.  The rest of the parameters are easy to convert.
+ *
+ * createSparkSpendTransaction: https://github.com/firoorg/sparkmobile/blob/23099b0d9010a970ad75b9cfe05d568d634088f3/src/spark.cpp#L190
+ */
+EXPORT_DART
+unsigned char* cCreateSparkSpendTransaction(
+    const char* keyDataHex,
+    int index,
+    struct CRecip* recipients, // This CRecip(ient) is not the same as a CRecipient.
+    int recipientsLength,
+    struct COutputRecipient* privateRecipients,
+    int privateRecipientsLength,
+    struct CCSparkMintMeta* coins,
+    int coinsLength,
+    struct CCoverSets* cover_set_data_all,
+    int cover_set_data_allLength,
+    // idAndBlockHashes_all is unused in spark::createSparkSpendTransaction so we ignore it here.
+    const char* txHashSig, // A hex string.
+    int txHashSigLength,
+    uint64_t fee,
+    // spark's createSparkSpendTransaction returns void, writing to the serializedSpend parameter.
+    // We return it instead.
+    const OutputScript* outputScripts,
+    int outputScriptsLength
+) {
+    try {
+        // Derive the keys from the key data and index.
+        spark::SpendKey spendKey = createSpendKeyFromData(keyDataHex, index);
+        spark::FullViewKey fullViewKey(spendKey);
+        spark::IncomingViewKey incomingViewKey(fullViewKey);
+
+        // Convert CRecipient* recipients to std::vector<std::pair<CAmount, bool>> cppRecipients.
+        std::vector<std::pair<CAmount, bool>> cppRecipients;
+        for (int i = 0; i < recipientsLength; i++) {
+            cppRecipients.push_back(std::make_pair(recipients[i].amount, recipients[i].subtractFee));
+        }
+
+        // Convert COutputRecipient* privateRecipients to std::vector<std::pair<spark::OutputCoinData, bool>> cppPrivateRecipients.
+        std::vector<std::pair<spark::OutputCoinData, bool>> cppPrivateRecipients;
+        for (int i = 0; i < privateRecipientsLength; i++) {
+            cppPrivateRecipients.push_back(std::make_pair(fromFFI(privateRecipients[i].output), privateRecipients[i].subtractFee));
+        }
+
+        // Convert CCSparkMintMeta* coins to std::list<CSparkMintMeta> cppCoins.
+        std::list<CSparkMintMeta> cppCoins;
+        for (int i = 0; i < coinsLength; i++) {
+            cppCoins.push_back(fromFFI(coins[i]));
+        }
+
+        // Convert CCoverSets* cover_set_data_all to a std::unordered_map<uint64_t, spark::CoverSetData> cppCoverSetDataAll
+        std::unordered_map<uint64_t, spark::CoverSetData> cppCoverSetDataAll;
+        for (int i = 0; i < cover_set_data_allLength; i++) {
+            for (int j = 0; j < cover_set_data_all[i].cover_setsLength; j++) {
+                // Convert CCoverSetData to vector of Coins.
+                std::vector<spark::Coin> cppCoverSetCoins;
+                spark::Coin coin = fromFFI(*cover_set_data_all[i].cover_sets[j].cover_set[0]);
+                cppCoverSetCoins.push_back(coin);
+
+                // Construct vector of vector of unsigned chars.
+                std::vector<CCoverSetData> coverSets(cover_set_data_all[i].cover_sets, cover_set_data_all[i].cover_sets + cover_set_data_all[i].cover_setsLength);
+
+                std::vector<std::vector<unsigned char>> coverSetReps;
+                for (auto& coverSet : coverSets) {
+                    std::vector<unsigned char> rep(coverSet.cover_set_representation,
+                                                   coverSet.cover_set_representation +
+                                                   coverSet.cover_set_representationLength);
+                    coverSetReps.push_back(rep);
+                }
+
+                spark::CoverSetData cppCoverSetData = createCoverSetData(cppCoverSetCoins, coverSetReps);
+                cppCoverSetDataAll[cover_set_data_all[i].cover_sets[j].cover_setLength] = cppCoverSetData;
+            }
+        }
+
+        // Convert const char * txHashSig to a uint256 cppTxHashSig.
+        // uint256 cppTxHashSig(txHashSig); // This throws `error: no matching function for call to ‘uint256::uint256(const char*&)’`, we need to:
+        uint256 cppTxHashSig;
+        cppTxHashSig.SetHex(txHashSig);
+
+        // Make a CAmount cppFee from fee.
+        CAmount cppFee = fee;
+
+        // Make a dummy for idAndBlockHashes_all.
+        std::map<uint64_t, uint256> cppIdAndBlockHashesAll;
+
+        // Make a placeholder for serializedSpend.
+        std::vector<uint8_t> cppSerializedSpend;
+
+        // Convert outputScripts to std::vector<std::vector<unsigned char>> cppOutputScripts.
+        std::vector<std::vector<unsigned char>> cppOutputScripts;
+        for (int i = 0; i < outputScriptsLength; i++) {
+            for (int j = 0; j < outputScripts[i].length; j++) {
+                cppOutputScripts[i].push_back(outputScripts[i].bytes[j]);
+            }
+        }
+
+        // Call spark::createSparkSpendTransaction.
+        createSparkSpendTransaction(
+            spendKey,
+            fullViewKey,
+            incomingViewKey,
+            cppRecipients,
+            cppPrivateRecipients,
+            cppCoins,
+            cppCoverSetDataAll,
+            cppIdAndBlockHashesAll,
+            cppTxHashSig,
+            cppFee,
+            cppSerializedSpend,
+            cppOutputScripts
+        );
+
+        // Allocate memory for the C-style string.
+        unsigned char* cstr = new unsigned char[cppSerializedSpend.size()];
+
+        // Copy the data from the vector to the array.
+        for (int i = 0; i < cppSerializedSpend.size(); i++) {
+            cstr[i] = cppSerializedSpend[i];
+        }
+
+        // Return the array.
+        return cstr;
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return nullptr;
+    }
 }
--- src/dart_interface.h
+++ src/dart_interface.h
@@ -108,6 +108,20 @@ struct CCRecipient* createSparkMintRecipients(
     int generate);
 
 /*
+ * FFI-friendly wrapper for a std::pair<CAmount, bool>.
+ *
+ * Note this is an ambiguation of a spark::CRecipient.  This CRecip(ient) is just a wrapper for a
+ * CAmount and bool pair, and is not the same as the spark::CRecipient struct above, which gets
+ * wrapped for us as a CCRecipient and is unrelated to this struct.
+ *
+ * See https://github.com/firoorg/sparkmobile/blob/23099b0d9010a970ad75b9cfe05d568d634088f3/src/spark.cpp#L190
+ */
+struct CRecip {
+    uint64_t amount;
+    int subtractFee;
+};
+
+/*
  * FFI-friendly wrapper for a spark::OutputCoinData.
  *
  * OutputCoinData: https://github.com/firoorg/sparkmobile/blob/8bf17cd3deba6c3b0d10e89282e02936d7e71cdd/src/spend_transaction.h#L33
@@ -116,6 +130,16 @@ struct COutputCoinData {
     const char* address;
     uint64_t value;
     const char* memo;
+};
+
+/*
+ * FFI-friendly wrapper for a <spark::OutputCoinData, bool>.
+ *
+ * See https://github.com/firoorg/sparkmobile/blob/23099b0d9010a970ad75b9cfe05d568d634088f3/src/spark.cpp#L195
+ */
+struct COutputRecipient {
+    struct COutputCoinData output;
+    int subtractFee;
 };
 
 /*
@@ -156,5 +180,44 @@ struct CCoverSetData {
     const unsigned char* cover_set_representation;
     int cover_set_representationLength;
 };
+
+/*
+ * FFI-friendly wrapper for a std::unordered_map<uint64_t, spark::CoverSetData>.
+ *
+ * See https://github.com/firoorg/sparkmobile/blob/23099b0d9010a970ad75b9cfe05d568d634088f3/src/spark.cpp#L197
+ */
+struct CCoverSets {
+    struct CCoverSetData* cover_sets;
+    int cover_setsLength;
+};
+
+struct OutputScript {
+    unsigned char* bytes;
+    int length;
+};
+
+/*
+ * FFI-friendly wrapper for spark::createSparkSpendTransaction.
+ *
+ * createSparkSpendTransaction: https://github.com/firoorg/sparkmobile/blob/23099b0d9010a970ad75b9cfe05d568d634088f3/src/spark.cpp#L190
+ */
+EXPORT_DART
+unsigned char* cCreateSparkSpendTransaction(
+    const char* keyDataHex,
+    int index,
+    struct CRecip* recipients,
+    int recipientsLength,
+    struct COutputRecipient* privateRecipients,
+    int privateRecipientsLength,
+    struct CCSparkMintMeta* coins,
+    int coinsLength,
+    struct CCoverSets* cover_set_data_all,
+    int cover_set_data_allLength,
+    const char* txHashSig,
+    int txHashSigLength,
+    uint64_t fee,
+    const OutputScript* outputScripts,
+    int outputScriptsLength
+);
 
 #endif //ORG_FIRO_SPARK_DART_INTERFACE_H
