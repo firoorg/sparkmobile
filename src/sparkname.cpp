3899f66f4693a0f71da9a23e7d6e90c45857dc42
levoncrypto <95240473+levoncrypto@users.noreply.github.com>
2025-02-19 05:26:06 +0400

    Script for spark name (#3)
    * Ownership proof
    
    * Script for spark name
    
    * Move function declaration
    
    ---------
    
    Co-authored-by: levonpetrosyan93 <petrosyan.levon93@gmail.com>

---
+++ src/sparkname.cpp
@@ -0,0 +1,30 @@
+#include "sparkname.h"
+
+namespace spark {
+
+void GetSparkNameScript(CSparkNameTxData& sparkNameData,
+                        Scalar m,
+                        const spark::SpendKey& spendKey,
+                        const spark::IncomingViewKey& incomingViewKey,
+                        std::vector<unsigned char>& outputScript)
+{
+    outputScript.clear();
+
+    spark::Address sparkAddress(spark::Params::get_default());
+    spark::OwnershipProof ownershipProof;
+
+    sparkAddress.decode(sparkNameData.sparkAddress);
+    sparkAddress.prove_own(m, spendKey, incomingViewKey, ownershipProof);
+
+    CDataStream ownershipProofStream(SER_NETWORK, PROTOCOL_VERSION);
+    ownershipProofStream << ownershipProof;
+
+    sparkNameData.addressOwnershipProof.assign(ownershipProofStream.begin(), ownershipProofStream.end());
+
+    CDataStream sparkNameDataStream(SER_NETWORK, PROTOCOL_VERSION);
+    sparkNameDataStream << sparkNameData;
+
+    outputScript.insert(outputScript.end(), sparkNameDataStream.begin(), sparkNameDataStream.end());
+}
+
+}
