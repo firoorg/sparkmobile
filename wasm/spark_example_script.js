412d72051fe4d1447b311055442d9313ca4d76c3
Gevorg Voskanyan <gevbsd@gmail.com>
2025-05-16 19:02:08 +0400

    JS example code for coins identification and related functionality, primarily aimed at assisting balance calculation

--- wasm/spark_example_script.js
+++ wasm/spark_example_script.js
@@ -271,6 +271,220 @@ spark().then(
        Module._free(serialContextPointer);
        Module._free(outputsPointerArray);
        Module.ccall("js_freeRecipientVector", null, ["number"], [recipientsVectorPtr]);
+
+       // Example usage of `js_deserializeCoin`
+       const serializedCoin = new Uint8Array([0x00, 0x00, 0x50, 0x02, 0xf6, 0xbf, 0x32, 0xc0, 0x85, 0xf7, 0xd9, 0x6c, 0x85, 0x73, 0x1f, 0x0b, 0x64, 0x3e, 0xcf, 0x9f, 0xd8, 0x34, 0xd5, 0x09, 0xa1, 0x75, 0xf9, 0x90, 0xf6, 0xe9, 0xd1, 0x56, 0x56, 0x01, 0x00, 0x36, 0xb8, 0x34, 0x3a, 0x68, 0x22, 0xe0, 0x92, 0xc3, 0x2e, 0x9a, 0x38, 0xbf, 0x06, 0x21, 0x68, 0xd8, 0x92, 0x3a, 0xba, 0xf8, 0x50, 0x85, 0x1f, 0xed, 0xa4, 0xe2, 0x4c, 0x7b, 0x32, 0x48, 0xf9, 0x01, 0x00, 0xdb, 0xd8, 0xd3, 0xf5, 0x44, 0x1a, 0xf4, 0x1b, 0x35, 0xed, 0xd5, 0x50, 0xbd, 0xce, 0x6a, 0xe9, 0x08, 0xb3, 0x39, 0x57, 0x1b, 0x19, 0x81, 0x0e, 0xf7, 0x43, 0xc4, 0x4c, 0x62, 0x45, 0x39, 0xfd, 0x01, 0x00, 0x52, 0x8d, 0xa6, 0x79, 0x2d, 0x16, 0xf9, 0xae, 0xe7, 0xdb, 0x3a, 0x05, 0x86, 0xdf, 0x5b, 0x57, 0xef, 0x4d, 0x11, 0xce, 0x42, 0xcc, 0x90, 0x25, 0x65, 0x3f, 0xbb, 0x88, 0x4a, 0x2b, 0x0b, 0x6f, 0x30, 0xd7, 0xc0, 0xd9, 0x11, 0x80, 0x6c, 0xf9, 0x4a, 0xa0, 0x5e, 0xfc, 0xe3, 0x7b, 0x8a, 0x1a, 0xed, 0x39, 0xf1, 0x1f, 0x37, 0xca, 0x69, 0x11, 0x47, 0x6c, 0x2f, 0x42, 0x5c, 0xdc, 0x0a, 0x3f, 0xc3, 0x7b, 0xa2, 0x7b, 0xdb, 0x7a, 0x6f, 0x7e, 0xc4, 0xe4, 0x47, 0x82, 0x1e, 0xda, 0x8d, 0xfa, 0xd9, 0x84, 0x3f, 0x10, 0xe6, 0x54, 0x2d, 0xc7, 0x24, 0x07, 0xc8, 0x7b, 0x2b, 0x10, 0xcd, 0x48, 0x79, 0xaf, 0xd8, 0xb9, 0x20, 0xf0, 0x6b, 0xec, 0x3a, 0xfa, 0x26, 0x2a, 0xb5, 0x6d, 0xd5, 0xbe, 0x88, 0x02, 0xec, 0xd3, 0x63, 0xdd, 0x68, 0x00, 0xb5, 0xa2, 0x13, 0x70, 0x89, 0xe5, 0xe3, 0xe8, 0x36, 0x5f, 0xea, 0xca, 0x27, 0xa0, 0x39, 0xa0, 0x12, 0x00, 0x00, 0x00, 0x00, 0x65, 0x9a, 0xce, 0x99, 0xbb, 0x84, 0x04, 0x5f, 0xd4, 0x2b, 0x9b, 0xbe, 0xa3, 0x8c, 0x5e, 0x5f, 0xc2, 0x91, 0x39, 0xb8, 0x79, 0x1f, 0x54, 0x7a, 0xa2, 0xfe, 0xb4, 0xc2, 0x2e, 0x1f, 0x0c, 0xae, 0x29, 0x91, 0x1a, 0x5d, 0x68, 0x2b, 0x02, 0x7f, 0x2e, 0x19, 0x30, 0xef, 0x40, 0xf5, 0xd4, 0xeb, 0xd7, 0xdc, 0x3d, 0x59, 0xf9, 0x3b, 0x2c, 0xf6, 0xda, 0x59, 0xf8, 0xac, 0x01, 0x0c, 0xfd, 0x58, 0xe2, 0x01, 0x00, 0x00, 0x00, 0x00, 0xfe, 0xff, 0xff, 0xff]);
+       const serializedCoinPointer = Module._malloc(serializedCoin.length);
+       Module.HEAPU8.set(serializedCoin, serializedCoinPointer);
+       const deserializedCoinObj = Module.ccall(
+           "js_deserializeCoin",            // C++ function name
+           "number",                        // Return type
+           ["number", "number"],            // Argument types
+           [serializedCoinPointer, serializedCoin.length] // Arguments
+       );
+       if (!deserializedCoinObj) {
+           throw new Error('Failed to deserialize coin.');
+       }
+       console.log("Deserialized Coin:", deserializedCoinObj);
+
+       // Example usage of `js_getMetadata`
+       const metadataObj = Module.ccall(
+           "js_getMetadata",               // C++ function name
+           "number",                       // Return type
+           ["number", "number"],           // Argument types
+           [deserializedCoinObj, incomingViewKeyObj] // Arguments
+       );
+       if (!metadataObj) {
+           console.log('Failed to get metadata from coin.');
+       }
+       else {
+          console.log("Coin Metadata:", metadataObj);
+
+          // Example usage of `js_getCoinFromMeta`
+          const coinFromMetaObj = Module.ccall(
+              "js_getCoinFromMeta",          // C++ function name
+              "number",                      // Return type
+              ["number", "number"],          // Argument types
+              [metadataObj, incomingViewKeyObj] // Arguments
+          );
+          if (!coinFromMetaObj) {
+              throw new Error('Failed to get coin from metadata.');
+          }
+          console.log("Coin from Metadata:", coinFromMetaObj);
+
+          // Example usage of `js_getInputData`
+          const inputDataObj = Module.ccall(
+              "js_getInputData",                                     // C++ function name
+              "number",                                             // Return type
+              ["number", "number", "number"],                       // Argument types
+              [deserializedCoinObj, fullViewKeyObj, incomingViewKeyObj] // Arguments
+          );
+          if (!inputDataObj) {
+              throw new Error('Failed to get input data.');
+          }
+          console.log("Input Data:", inputDataObj);
+
+          // Get Input Coin Data Cover Set ID
+          const inputCoverSetId = Module.ccall(
+              "js_getInputCoinDataCoverSetId", // C++ function name
+              "number",                        // Return type
+              ["number"],                      // Argument types
+              [inputDataObj]                   // Arguments
+          );
+          console.log("Input Coin Data Cover Set ID:", inputCoverSetId);
+          
+          // Get Input Coin Data Index
+          const inputIndex = Module.ccall(
+              "js_getInputCoinDataIndex",     // C++ function name
+              "number",                       // Return type
+              ["number"],                     // Argument types
+              [inputDataObj]                  // Arguments
+          );
+          console.log("Input Coin Data Index:", inputIndex);
+          
+          // Get Input Coin Data Value
+          const inputValue = Module.ccall(
+              "js_getInputCoinDataValue",     // C++ function name
+              "number",                       // Return type
+              ["number"],                     // Argument types
+              [inputDataObj]                  // Arguments
+          );
+          console.log("Input Coin Data Value:", inputValue);
+          
+          // Get Input Data With Meta
+          const inputDataWithMetaObj = Module.ccall(
+              "js_getInputDataWithMeta",      // C++ function name
+              "number",                       // Return type
+              ["number", "number", "number"], // Argument types
+              [deserializedCoinObj, metadataObj, fullViewKeyObj] // Arguments
+          );
+          if (!inputDataWithMetaObj) {
+              throw new Error('Failed to get input data with metadata.');
+          }
+          console.log("Input Data with Metadata:", inputDataWithMetaObj);
+
+          // Free allocated memory
+          Module.ccall("js_freeInputCoinData", null, ["number"], [inputDataWithMetaObj]);
+          Module.ccall("js_freeInputCoinData", null, ["number"], [inputDataObj]);
+       }
+
+       // Example usage of `js_identifyCoin`
+       const identifiedCoinObj = Module.ccall(
+           "js_identifyCoin",             // C++ function name
+           "number",                      // Return type
+           ["number", "number"],          // Argument types
+           [deserializedCoinObj, incomingViewKeyObj] // Arguments
+       );
+       if (!identifiedCoinObj) {
+           console.log('Failed to identify coin.');
+       }
+       else {
+          console.log("Identified Coin:", identifiedCoinObj);
+
+          // Get Identified Coin Diversifier
+          const coinDiversifier = Module.ccall(
+              "js_getIdentifiedCoinDiversifier", // C++ function name
+              "number",                          // Return type
+              ["number"],                        // Argument types
+              [identifiedCoinObj]                // Arguments
+          );
+          console.log("Identified Coin Diversifier:", coinDiversifier);
+          
+          // Get Identified Coin Value
+          const value = Module.ccall(
+              "js_getIdentifiedCoinValue",      // C++ function name
+              "number",                         // Return type
+              ["number"],                       // Argument types
+              [identifiedCoinObj]               // Arguments
+          );
+          console.log("Identified Coin Value:", value);
+          
+          // Get Identified Coin Memo
+          const memo = Module.ccall(
+              "js_getIdentifiedCoinMemo",       // C++ function name
+              "string",                         // Return type
+              ["number"],                       // Argument types
+              [identifiedCoinObj]               // Arguments
+          );
+          console.log("Identified Coin Memo:", memo);
+       }
+
+       // Get Spark Mint Meta Height
+       const metaHeight = Module.ccall(
+           "js_getCSparkMintMetaHeight",  // C++ function name
+           "number",                      // Return type
+           ["number"],                    // Argument types
+           [metadataObj]                  // Arguments
+       );
+       console.log("Spark Mint Meta Height:", metaHeight);
+       
+       // Get Spark Mint Meta ID
+       const metaId = Module.ccall(
+           "js_getCSparkMintMetaId",     // C++ function name
+           "number",                     // Return type
+           ["number"],                   // Argument types
+           [metadataObj]                 // Arguments
+       );
+       console.log("Spark Mint Meta ID:", metaId);
+
+       // Get Spark Mint Meta Is Used
+       const metaIsUsed = Module.ccall(
+           "js_getCSparkMintMetaIsUsed", // C++ function name
+           "number",                     // Return type
+           ["number"],                   // Argument types
+           [metadataObj]                 // Arguments
+       );
+       console.log("Spark Mint Meta Is Used:", metaIsUsed !== 0);
+
+       // Get Spark Mint Meta Memo
+       const metaMemo = Module.ccall(
+           "js_getCSparkMintMetaMemo",   // C++ function name
+           "string",                     // Return type
+           ["number"],                   // Argument types
+           [metadataObj]                 // Arguments
+       );
+       console.log("Spark Mint Meta Memo:", metaMemo);
+
+       // Get Spark Mint Meta Diversifier
+       const metaDiversifier = Module.ccall(
+           "js_getCSparkMintMetaDiversifier", // C++ function name
+           "number",                          // Return type
+           ["number"],                        // Argument types
+           [metadataObj]                      // Arguments
+       );
+       console.log("Spark Mint Meta Diversifier:", metaDiversifier);
+
+       // Get Spark Mint Meta Value
+       const metaValue = Module.ccall(
+           "js_getCSparkMintMetaValue",  // C++ function name
+           "number",                     // Return type
+           ["number"],                   // Argument types
+           [metadataObj]                 // Arguments
+       );
+       console.log("Spark Mint Meta Value:", metaValue);
+
+       // Get Spark Mint Meta Type
+       const metaType = Module.ccall(
+           "js_getCSparkMintMetaType",   // C++ function name
+           "number",                     // Return type
+           ["number"],                   // Argument types
+           [metadataObj]                 // Arguments
+       );
+       console.log("Spark Mint Meta Type:", metaType);
+       
+       // Get Spark Mint Meta Coin
+       const metaCoinObj = Module.ccall(
+           "js_getCSparkMintMetaCoin",   // C++ function name
+           "number",                     // Return type
+           ["number"],                   // Argument types
+           [metadataObj]                 // Arguments
+       );
+       console.log("Spark Mint Meta Coin:", metaCoinObj);
+
+       // Free allocated memory
+       Module.ccall("js_freeCoin", null, ["number"], [deserializedCoinObj]);
+       Module.ccall("js_freeIdentifiedCoinData", null, ["number"], [identifiedCoinObj]);
+       Module.ccall("js_freeCSparkMintMeta", null, ["number"], [metadataObj]);
        Module.ccall("js_freeAddress", null, ["number"], [addressObj]);
        Module.ccall("js_freeIncomingViewKey", null, ["number"], [incomingViewKeyObj]);
        Module.ccall("js_freeFullViewKey", null, ["number"], [fullViewKeyObj]);
